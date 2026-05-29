e14a65728b0a1cc45ad2fa12db5226729b21cac1
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-05-29 11:23:43 +0200

    Initial commit

---
+++ src/test/java/edu/se/bowling/BowlingGameTest.java
@@ -0,0 +1,19 @@
+package edu.se.bowling;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class BowlingGameTest {
+
+	@BeforeEach
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
