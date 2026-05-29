ca727c16232744c6c96435c9f9e8dce7e12cbfe6
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-05-29 11:35:30 +0200

    test: gutter game

---
+++ src/main/java/edu/se/bowling/Game.java
@@ -0,0 +1,11 @@
+package edu.se.bowling;
+
+public class Game {
+
+    public void roll(int pins) {
+    }
+
+    public int score() {
+        return -1;
+    }
+}
--- src/test/java/edu/se/bowling/BowlingGameTest.java
+++ src/test/java/edu/se/bowling/BowlingGameTest.java
@@ -3,17 +3,27 @@ package edu.se.bowling;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class BowlingGameTest {
+	private Game game;
 
 	@BeforeEach
 	public void setUp() throws Exception {
+		game = new Game();
+	}
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++) {
+			game.roll(pins);
+		}
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGutterGame() {
+		rollMany(20, 0);
+		assertEquals(0, game.score());
 	}
 
 }
