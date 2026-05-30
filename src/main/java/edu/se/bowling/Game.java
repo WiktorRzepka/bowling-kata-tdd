0aa5d85a2e5d461e8c58edf0aaf15a60b86e38fa
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-05-30 14:30:43 +0200

    refactor: add static MAX_NUMBER_OF_ROLLS

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -2,7 +2,8 @@ package edu.se.bowling;
 
 public class Game {
 
-    private int[] rolls = new int[21];
+    private static final int MAX_NUMBER_OF_ROLLS = 21;
+    private int[] rolls = new int[MAX_NUMBER_OF_ROLLS];
     private int currentRoll = 0;
 
     public void roll(int pins) {
