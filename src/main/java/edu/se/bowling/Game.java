85a8568b2e6c23da830bf9941007b11505c7f384
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-05-30 14:28:45 +0200

    feat: implement basic roll and score methods

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -2,10 +2,15 @@ package edu.se.bowling;
 
 public class Game {
 
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
     public void roll(int pins) {
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        return -1;
+        int result = 0;
+        return result;
     }
 }
